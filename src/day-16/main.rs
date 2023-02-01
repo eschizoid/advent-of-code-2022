663ee4e5b6555fa11060f462be5180984357a67d
mariano <mariano.gonzalez.mx@gmail.com>
2023-01-31 18:49:01 -0600

    Add solution for day 16

--- src/day-16/main.rs
+++ src/day-16/main.rs
@@ -1,3 +1,66 @@
+use nom::branch::alt;
+use nom::bytes::complete::take_while;
+use nom::character::complete as cc;
+use nom::combinator::{all_consuming, map_res};
+use nom::multi::separated_list1;
+use nom::sequence::{separated_pair, tuple};
+use nom::{bytes::complete::tag, Finish, IResult};
+
+#[derive(Debug)]
+struct Valve {
+  name: String,
+  flow_rate: i32,
+  tunnel_valves: Vec<String>,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let valves = all_consuming(parse_all_valves)(include_str!("input.txt"))
+    .finish()
+    .unwrap()
+    .1;
+  println!("{:?}", valves);
+}
+
+fn parse_all_valves(i: &str) -> IResult<&str, Vec<Valve>> {
+  return separated_list1(cc::newline, parse_valve)(i);
+}
+
+fn parse_valve(input: &str) -> IResult<&str, Valve> {
+  // Valve QJ has flow rate=11; tunnels lead to valves HB, GL
+  let (input, result) = separated_pair(
+    tuple((
+      tag("Valve "),
+      map_res(take_while(|c: char| c.is_alphabetic()), |s: &str| {
+        s.parse::<String>()
+      }),
+      tag(" has flow rate="),
+      map_res(take_while(|c: char| c.is_numeric()), |s: &str| {
+        s.parse::<i32>()
+      }),
+    )),
+    tag("; "),
+    tuple((
+      alt((tag("tunnels "), tag("tunnel "))),
+      alt((tag("lead "), tag("leads "))),
+      alt((tag("to valves "), tag("to valve "))),
+      separated_list1(
+        tag(", "),
+        map_res(take_while(|c: char| c.is_alphabetic()), |s: &str| {
+          s.parse::<String>()
+        }),
+      ),
+    )),
+  )(input)?;
+
+  Ok((
+    input,
+    Valve {
+      #[rustfmt::skip]
+      name: result.0.1,
+      #[rustfmt::skip]
+      flow_rate: result.0.3,
+      #[rustfmt::skip]
+      tunnel_valves: result.1.3,
+    },
+  ))
 }
